517dbaeeb9649486696fd9d7555b5b50f338348a
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2018-12-10 13:19:48 -0800

    Added more fields to BookVolume model.

--- app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/BookVolume.java
+++ app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/BookVolume.java
@@ -1,14 +1,42 @@
 package com.joshuahalvorson.android_projectweek1_googlebooks;
 
 public class BookVolume {
-    private String title, imageUrl, userReview;
+    private String title, imageUrl, userReview, authors, publishedDate;
+    private int pages;
     private boolean hasRead;
 
-    public BookVolume(String title, String imageUrl, String userReview, boolean hasRead) {
+    public BookVolume(String title, String imageUrl, String userReview, String authors, String publishedDate, int pages, boolean hasRead) {
         this.title = title;
         this.imageUrl = imageUrl;
         this.userReview = userReview;
+        this.authors = authors;
+        this.publishedDate = publishedDate;
+        this.pages = pages;
         this.hasRead = hasRead;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
     }
 
     public String getTitle() {
