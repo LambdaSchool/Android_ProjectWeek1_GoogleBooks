486a0b0f96bf030d9b0e53b0f79676ccb8cfa9f2
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2018-12-11 00:26:30 -0800

    Add search results to simple scroll view.

--- app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/GoogleBooksApiDao.java
+++ app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/GoogleBooksApiDao.java
@@ -12,41 +12,36 @@ public class GoogleBooksApiDao {
 
     public static ArrayList<BookVolume> getBooksFromSearch(final String search){
         final ArrayList<BookVolume> books = new ArrayList<>();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                final String results = NetworkAdapter.httpRequest(SEARCH_URL + search, NetworkAdapter.GET);
-                try {
-                    JSONObject topLevel = new JSONObject(results);
-                    JSONArray booksArray = topLevel.getJSONArray(BOOKS_ARRAY_IN_API);
-                    for(int i = 0; i < booksArray.length(); i++){
-                        JSONObject bookObject = booksArray.getJSONObject(i);
-                        JSONObject volumeInfo = bookObject.getJSONObject("volumeInfo");
-                        JSONObject imageUrls = volumeInfo.getJSONObject("imageLinks");
-                        String authors = "";
-                        if(volumeInfo.has("authors")){
-                            JSONArray authorsArray = volumeInfo.getJSONArray("authors");
-                            if(authorsArray != null){
-                                for(int j = 0; j < authorsArray.length(); j++){
-                                    authors += authorsArray.get(j) + ", ";
-                                }
-                            }
+        final String results = NetworkAdapter.httpRequest(SEARCH_URL + search, NetworkAdapter.GET);
+        try {
+            JSONObject topLevel = new JSONObject(results);
+            JSONArray booksArray = topLevel.getJSONArray(BOOKS_ARRAY_IN_API);
+            for(int i = 0; i < booksArray.length(); i++){
+                JSONObject bookObject = booksArray.getJSONObject(i);
+                JSONObject volumeInfo = bookObject.getJSONObject("volumeInfo");
+                JSONObject imageUrls = volumeInfo.getJSONObject("imageLinks");
+                String authors = "";
+                if(volumeInfo.has("authors")){
+                    JSONArray authorsArray = volumeInfo.getJSONArray("authors");
+                    if(authorsArray != null){
+                        for(int j = 0; j < authorsArray.length(); j++){
+                            authors += authorsArray.get(j) + ", ";
                         }
-                        String title = volumeInfo.getString("title");
-                        String imageUrl = imageUrls.getString("thumbnail");
-                        String publishedDate = volumeInfo.getString("publishedDate");
-                        int pages = 0;
-                        if(volumeInfo.has("pageCount")){
-                            pages = volumeInfo.getInt("pageCount");
-                        }
-                        BookVolume book = new BookVolume(title, imageUrl, null, authors, publishedDate, pages, false);
-                        books.add(book);
                     }
-                } catch (JSONException e) {
-                    e.printStackTrace();
                 }
+                String title = volumeInfo.getString("title");
+                String imageUrl = imageUrls.getString("thumbnail");
+                String publishedDate = volumeInfo.getString("publishedDate");
+                int pages = 0;
+                if(volumeInfo.has("pageCount")){
+                    pages = volumeInfo.getInt("pageCount");
+                }
+                BookVolume book = new BookVolume(title, imageUrl, null, authors, publishedDate, pages, false);
+                books.add(book);
             }
-        }).start();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return books;
     }
 }
--- app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/HomeFragment.java
+++ app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/HomeFragment.java
@@ -8,9 +8,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
-
+    private EditText searchText;
+    private TextView resultsContainer;
+    private Button searchButton;
+    ArrayList<BookVolume> booksList;
     public HomeFragment(){
 
     }
@@ -34,11 +43,33 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        searchText = view.findViewById(R.id.search_for_book_edit_text);
+        resultsContainer = view.findViewById(R.id.search_results_container);
+        searchButton = view.findViewById(R.id.search_button);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+         searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        booksList = GoogleBooksApiDao.getBooksFromSearch(searchText.getText().toString());
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                for(int i = 0; i < booksList.size(); i++){
+                                    resultsContainer.setText(resultsContainer.getText().toString() + booksList.get(i).getTitle() + '\n');
+                                }
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
     }
 
     @Override
