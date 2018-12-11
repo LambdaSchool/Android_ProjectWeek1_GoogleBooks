c26ed6b63d1594e1d930370203a8b667b975590a
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2018-12-11 14:56:55 -0800

    Can set has read with checkbox in dialog. Checkbox updates when started.

--- app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/EditBookDialogFragment.java
+++ app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/EditBookDialogFragment.java
@@ -1,6 +1,7 @@
 package com.joshuahalvorson.android_projectweek1_googlebooks;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -50,6 +51,25 @@ public class EditBookDialogFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        if(bookVolume.isHasRead() == 1){
+            setHasReadCheckBox.setChecked(true);
+        }else{
+            setHasReadCheckBox.setChecked(false);
+        }
+        setHasReadCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(setHasReadCheckBox.isChecked()){
+                    bookVolume.setHasRead(1);
+                    BookVolumeViewModel.updateBook(bookVolume);
+                    Log.i("onClickCheckBock", "book read update to true");
+                }else{
+                    bookVolume.setHasRead(0);
+                    BookVolumeViewModel.updateBook(bookVolume);
+                    Log.i("onClickCheckBock", "book read update to false");
+                }
+            }
+        });
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
