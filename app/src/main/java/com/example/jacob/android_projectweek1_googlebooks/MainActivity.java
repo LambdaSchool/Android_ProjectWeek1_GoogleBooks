38b5e3f1930f96ac231f623a918f9767261f21a4
Jacob Seppanen <jbseppanen@gmail.com>
2018-12-10 17:02:45 -0800

    Add async task to main activity to test results from search.

--- app/src/main/java/com/example/jacob/android_projectweek1_googlebooks/MainActivity.java
+++ app/src/main/java/com/example/jacob/android_projectweek1_googlebooks/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.jacob.android_projectweek1_googlebooks;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    Context context;
+    EditText editTextSearch;
+    ProgressBar progressBar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,9 +47,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context = this;
+
+        editTextSearch = findViewById(R.id.edit_text_search);
+        progressBar = findViewById(R.id.progress_bar);
+
+        findViewById(R.id.button_search).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new getBooksTask().execute(editTextSearch.getText().toString());
+            }
+        });
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    public class getBooksTask extends AsyncTask<String, Integer, View> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(View view) {
+            super.onPostExecute(view);
+            progressBar.setVisibility(View.GONE);
+
+        }
+
+        @Override
+        protected View doInBackground(String... strings) {
+            ArrayList<Book> temp = BookGoogleApiDao.searchBooks(strings[0]);
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressBar.setProgress(values[0]);
+        }
+    }
 }
