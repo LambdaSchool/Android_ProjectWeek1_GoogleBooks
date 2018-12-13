c9ce838f2c64f48173eb5cd7446bc00a09827105
Jacob Seppanen <jbseppanen@gmail.com>
2018-12-12 21:34:12 -0800

    And BookView Activity and layout for viewing a single book and/or adding a review.

---
+++ app/src/main/java/com/example/jacob/android_projectweek1_googlebooks/BookViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.jacob.android_projectweek1_googlebooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_view);
+    }
+}
