5eff9efde61f1d2d5c81119628193e133295de38
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2018-12-12 17:59:34 -0800

    QR displayed toast message if buy link is not found.

--- app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/QrCodeDialogFragment.java
+++ app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/QrCodeDialogFragment.java
@@ -6,16 +6,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.StringUtils;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 public class QrCodeDialogFragment extends Fragment {
@@ -41,11 +44,12 @@ public class QrCodeDialogFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         bookVolume = getArguments().getParcelable("book");
-        if(bookVolume.getSaleLink() != null){
+        if(!TextUtils.isEmpty(bookVolume.getSaleLink())){
             QRCodeWriter writer = new QRCodeWriter();
             BitMatrix bitMatrix = null;
             try {
-                bitMatrix = writer.encode(bookVolume.getSaleLink(), BarcodeFormat.QR_CODE, 512, 512);
+                String saleLink = bookVolume.getSaleLink();
+                bitMatrix = writer.encode(saleLink, BarcodeFormat.QR_CODE, 512, 512);
                 int width = bitMatrix.getWidth();
                 int height = bitMatrix.getHeight();
                 Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
@@ -58,6 +62,11 @@ public class QrCodeDialogFragment extends Fragment {
             } catch (WriterException e) {
                 e.printStackTrace();
             }
+        }else{
+            getFragmentManager().popBackStack();
+            Toast.makeText(getContext(),
+                    "QR code could not be generated, buy link not found",
+                    Toast.LENGTH_LONG).show();
 
         }
     }
