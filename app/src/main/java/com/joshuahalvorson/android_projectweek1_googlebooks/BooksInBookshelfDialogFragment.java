cdffcfa1d134e2f5773a523dcae17f1b3f11c9db
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2018-12-12 17:46:52 -0800

    Books in bookshelf now displayed in recyclerview.

--- app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/BooksInBookshelfDialogFragment.java
+++ app/src/main/java/com/joshuahalvorson/android_projectweek1_googlebooks/BooksInBookshelfDialogFragment.java
@@ -1,10 +1,13 @@
 package com.joshuahalvorson.android_projectweek1_googlebooks;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +20,8 @@ public class BooksInBookshelfDialogFragment extends Fragment {
     ArrayList<BookVolume> bookVolumes;
     private Bookshelf bookshelf;
     private LinearLayout scrollView;
+    private RecyclerView recyclerView;
+    private BooksInBookshelfAdapter adapter;
 
     public BooksInBookshelfDialogFragment(){
     }
@@ -31,6 +36,7 @@ public class BooksInBookshelfDialogFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         scrollView = view.findViewById(R.id.scroll_view);
+        recyclerView = view.findViewById(R.id.books_in_shelf_recycler_view);
     }
 
     @Override
@@ -38,7 +44,12 @@ public class BooksInBookshelfDialogFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         bookshelf = getArguments().getParcelable("bookshelf");
         bookVolumes = BooksViewModel.readBooksInBookshelf(bookshelf);
-        scrollView.removeAllViews();
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new BooksInBookshelfAdapter(getActivity(), bookVolumes, bookshelf);
+        recyclerView.setAdapter(adapter);
+        new getBooksInShelf().execute();
+        /*scrollView.removeAllViews();
         for(int i = 0; i < bookVolumes.size(); i++){
             TextView tv = new TextView(getContext());
             final BookVolume bookVolume = bookVolumes.get(i);
@@ -54,6 +65,28 @@ public class BooksInBookshelfDialogFragment extends Fragment {
                 }
             });
             scrollView.addView(tv);
+        }*/
+    }
+
+    public class getBooksInShelf extends AsyncTask<Void, Integer, ArrayList<BookVolume>> {
+
+        @Override
+        protected ArrayList<BookVolume> doInBackground(Void... voids) {
+            return BooksViewModel.readBooksInBookshelf(bookshelf);
         }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<BookVolume> volumes) {
+            super.onPostExecute(volumes);
+            bookVolumes.clear();
+            bookVolumes.addAll(volumes);
+            adapter.notifyDataSetChanged();
+        }
+
     }
 }
