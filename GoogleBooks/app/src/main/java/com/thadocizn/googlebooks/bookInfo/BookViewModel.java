4edbe4b88aa0dc39c7f19420ecbb3cb21fa9dc27
ThaDocsN <charles.tha.doc@gamil.com>
2018-12-24 14:15:00 -0600

    added bookshelf viewModel

--- GoogleBooks/app/src/main/java/com/thadocizn/googlebooks/bookInfo/BookViewModel.java
+++ GoogleBooks/app/src/main/java/com/thadocizn/googlebooks/bookInfo/BookViewModel.java
@@ -9,22 +9,7 @@ import com.thadocizn.googlebooks.bookshelfInfo.Bookshelf;
 import java.util.ArrayList;
 
 public class BookViewModel extends ViewModel {
-
-    private MutableLiveData<ArrayList<Bookshelf>> shelfList;
     private BookRepository repo;
-
-    public LiveData<ArrayList<Bookshelf>> getBookshelfList(){
-        if (shelfList == null){
-            loadList();
-        }
-
-        return shelfList;
-    }
-
-    private void loadList() {
-        repo = new BookRepository();
-        shelfList = repo.getBookShelves();
-    }
 
     public void addBook(BookClass book) {
         repo = new BookRepository();
@@ -39,16 +24,5 @@ public class BookViewModel extends ViewModel {
         repo.updateBook(book);
     }
 
-    public void addBookshelf(String bookshelf) {
-        repo = new BookRepository();
-        repo.createBookshelf(bookshelf);
-    }
 
-    public void deleteBookshelf(Bookshelf bookshelf) {
-        repo.deleteBookshelf(bookshelf);
-    }
-
-    public void updateBookshelf(Bookshelf bookshelf) {
-        repo.updateBookshelf(bookshelf);
-    }
 }
---
+++ GoogleBooks/app/src/main/java/com/thadocizn/googlebooks/bookshelfInfo/BookshelfViewModel.java
@@ -0,0 +1,41 @@
+package com.thadocizn.googlebooks.bookshelfInfo;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.thadocizn.googlebooks.bookInfo.BookRepository;
+
+import java.util.ArrayList;
+
+public class BookshelfViewModel extends ViewModel {
+
+    private MutableLiveData<ArrayList<Bookshelf>> shelfList;
+    private BookshelfRepository repo;
+
+    public LiveData<ArrayList<Bookshelf>> getBookshelfList(){
+        if (shelfList == null){
+            loadList();
+        }
+
+        return shelfList;
+    }
+
+    private void loadList() {
+        repo = new BookshelfRepository();
+        shelfList = repo.getBookShelves();
+    }
+
+    public void addBookshelf(String bookshelf) {
+        repo = new BookshelfRepository();
+        repo.createBookshelf(bookshelf);
+    }
+
+    public void deleteBookshelf(Bookshelf bookshelf) {
+        repo.deleteBookshelf(bookshelf);
+    }
+
+    public void updateBookshelf(Bookshelf bookshelf) {
+        repo.updateBookshelf(bookshelf);
+    }
+}
